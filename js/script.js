8b0c34e4bb4ab40ba1443444e36b624e5de27097
ACE <acetip@aces-mbp.lab.lagcc>
2017-11-13 20:15:52 -0500

    something

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var laguardia = {
+  buildings: 4,
+  community: true,
+  location: "Long island city, queens",
+  facilities: ["library", "computer lab", "gym",200, true],
+  room: {
+    number: 19,
+    projector: true,
+    windows: null,
+    seats: "they're uncomfortable",
+  },
+  davidSays: function(name) {
+    console.log("This school is great" + name);
+  }
+};
+laguardia.davidSays("-zein");
+
+//this is a brand new object
+
+var myCellPhone = {
+  cameras: 2,
+  metroPCS: true,
+  
+}
